d1c5e46bf716887a64ff76079cebd44fd17ca138
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-06-07 08:52:54 -0400

    credenciales y firebase
    archivo credenciales y firebase completos

---
+++ assets/js/credenciales.js
@@ -0,0 +1,13 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBBx-uhcr_X2T4G775JJ7TNlIe2IzK5OLo",
+    authDomain: "evalaucion3.firebaseapp.com",
+    projectId: "evalaucion3",
+    storageBucket: "evalaucion3.appspot.com",
+    messagingSenderId: "461383595787",
+    appId: "1:461383595787:web:f0c5a308b819d51197940e"
+  };
+
+
+
+
