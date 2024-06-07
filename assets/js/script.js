8a0560951b67a11e5127f422c4f38828f5eff200
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-06-07 09:26:46 -0400

    agregamos registrar
    agregamos el registrar en el scrip y una funcion para verificar los valores entregados en los cheked radio

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+import { obtenerPersonas, registrarPersona, actualizarPersona, eliminarPersona } from "./promesas.js";
+
+//funcion para validar campos que usen radio de cualqueir formualario 
+//en est caso usada para el sexo
+//recorre con un for una lista creada con el valor del radio
+//luego usamos if si si el radio esta clickeado con .checked y le devolvemos el valor
+const obtenerValorRadio = (valores) => {
+    for (let i = 0; i < valores.length; i++){
+        if (valores[i].checked){
+            return valores[i].value;
+        }
+    }
+    return null; // si no seleeciono ningun radio es nulo
+  };
+
+//funcion para registrar personas se recuperan elementos se agrega valor y se crea el objeto
+const registrar = () => {
+    let eNombre = document.getElementById("nombre");
+    let eApellido = document.getElementById("apellido");
+    let eContraseña = document.getElementById("contraseña");
+    let eFecha = document.getElementById("fecha");
+    //uso la funcion class name porque al ser dos valores distintos debo meterlos en una clase ya que el id es solo para 1
+    let eSexo = document.getElementsByClassName("sexo");
+    let eCategoria = document.getElementById("categoria");
+    let eBici = document.getElementById("bici");
+    let eOpinion = document.getElementById("opinion");
+    //luego de definir y recuperar los elementos le agrupamos el valor
+    let vNombre = eNombre.value;
+    let vApellido = eApellido.value;
+    let vContraseña = eContraseña.value;
+    let vFecha = eFecha.value;
+    //utilizamos la funcion general de validaciones del radio
+    let vSexo = obtenerValorRadio(eSexo);
+    let Vcategoria = eCategoria.value;
+    let vBici = eBici.value;
+    let vOpinion = eOpinion.value;
+
+    //agrego una validacion para los campos (espero crear otra funcion que valide cada data ingresado al form)
+
+    if (vNombre === "" || vApellido === "" || vContraseña === "" || vFecha === "" || vSexo === null || Vcategoria === "" || vBici === "" || vOpinion === "") {
+        alert("Debe llenar todos los campos");
+        return;
+    }
+
+    //creamos el objeto
+    let objeto = {
+        nombre: vNombre,
+        apellido: vApellido,
+        contraseña:  vContraseña,
+        fecha: vFecha,
+        sexo: vSexo,
+        categoria: Vcategoria,
+        bici: vBici,
+        opinion: vOpinion,
+    };
+    console.log(objeto);
+
+};
